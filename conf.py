a0a8e95dccf165b708b156ff49ff8a7b6e1cf4aa
chenzake <zackchen517@gmail.com>
2018-02-08 17:26:13 +0800

    second commit

--- conf.py
+++ conf.py
@@ -82,8 +82,23 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_bootstrap_theme
+#html_theme = 'bootstrap'
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path() 
+#html_theme_options = {  
+#    'navbar_title': "Sandglass",  
+#    'globaltoc_depth': 2,  
+#    'globaltoc_includehidden': "true",  
+#    'navbar_class': "navbar navbar-inverse",  
+#    'navbar_fixed_top': "true",  
+#    'bootswatch_theme': "united",  
+#    'bootstrap_version': "3",  
+#}
+import sphinx_rtd_theme
 
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
